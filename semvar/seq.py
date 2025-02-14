59ab75af8a4db6f079e0006d2c95d6e6b804671e
Rintsen Sherpa <rintsen@umich.edu>
2025-02-14 16:46:32 -0500

    Edited docstring

--- semvar/seq.py
+++ semvar/seq.py
@@ -1,16 +1,16 @@
 def get_sequences(sem_len, fasta, chr, pos, ref, alt):
-    '''Get all possible reference and variant k-mers overlapping the variant position where k is the length of the SEM
+    '''Get the sequence windows around the variant to be scanned by the SEM
     Params
         sem_len (int): length of SEM
         fasta (pyfaidx.Fasta): Pyfaidx Fasta object of the reference genome assembly
         chr (str): Chromosome of SNV in 'chrN' format
         pos (int): 1-based position of SNV
-        ref (str): Reference allele
+        ref (str): Reference allele # To be used in indel implementation
         alt (str): Alternate allele
     Returns
-        kmers (dict of lists): Lists of k-mers and their coordinates
+        ref_seq, alt_seq (str, str): Ref and alt sequences to be scanned by the SEM
     '''
-    seq_start = pos-sem_len # currently pos is 1-based
+    seq_start = pos-sem_len # pos is 1-based
     seq_end = pos+sem_len-1
     ref_seq = fasta[chr][seq_start:seq_end].seq.upper()
     middle_index = sem_len - 1
